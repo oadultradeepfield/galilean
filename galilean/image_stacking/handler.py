9c138b9ad3ad502933e2c7afe37c047b7753ed21
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2024-12-28 15:20:10 +0700

    chore: adjust default value for consistency

--- galilean/image_stacking/handler.py
+++ galilean/image_stacking/handler.py
@@ -6,7 +6,7 @@ from image_stacking.utils import (mean_image_stacking,
                                   median_image_stacking)
 
 
-def image_stacking(images: np.ndarray, method: str = "mean") -> np.ndarray:
+def image_stacking(images: np.ndarray, method: str = "mean_with_median_clipping") -> np.ndarray:
     """Stack and perform superresolution on images using the specified method."""
     method_map = {
         "mean": mean_image_stacking,
