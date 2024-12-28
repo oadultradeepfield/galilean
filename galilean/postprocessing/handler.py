416ed569f89f11e007a8371c79c2d99c3439e3ab
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2024-12-28 17:08:28 +0700

    feat: improve postprocessing by scaling at the last step

--- galilean/postprocessing/handler.py
+++ galilean/postprocessing/handler.py
@@ -5,10 +5,11 @@ from postprocessing.utils import (calibrate_color, images_sr,
 
 
 def postprocessing(image: np.ndarray, sharpening_factor: float = 1.5, scaling_factor: int = 2) -> np.ndarray:
-    """Processes color planetary image with calibration and sharpening"""
-    if scaling_factor > 1:
-        image = images_sr(image, scaling_factor)
-        
+    """Processes color planetary image with calibration, sharpening, and super resolution"""
     processed = calibrate_color(image)
     processed = selective_sharpening(processed, sharpening_factor)
+    
+    if scaling_factor > 1:
+        processed = images_sr(processed, scaling_factor)
+        
     return processed
